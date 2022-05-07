11bdbad0bdb444e9efaebbe4450e3645dc4f25b4
Volha Lytkina <voly0621@netcracker.com>
2022-05-07 22:00:33 +0300

    init spring boot

---
+++ MigrationMonitoringTool/src/main/java/by/bntu/fitr/poisit/lytkina/MigrationMonitoringTool/MigrationMonitoringToolApplication.java
@@ -0,0 +1,13 @@
+package by.bntu.fitr.poisit.lytkina.MigrationMonitoringTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationMonitoringToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationMonitoringToolApplication.class, args);
+	}
+
+}
