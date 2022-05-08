34b558eb3dd096e2d5bd6d3dbd70c18e0a43e0a3
Volha Lytkina <voly0621@netcracker.com>
2022-05-08 23:15:18 +0300

    init material-ui

---
+++ migration-frontend/src/Dashboard.js
@@ -0,0 +1,10 @@
+import Header from './components/Header';
+import './App.css';
+
+function Dashboard() {
+  return (
+    <Header />
+  );
+}
+
+export default Dashboard;
---
+++ migration-frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Header() {    
+      return (
+        <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            FLOWS
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      );
+    }
